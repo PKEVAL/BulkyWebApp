ec0f73e4b5d943e873d6936db100c8b8a8bb51e3
PKEVAL <kevalpatel80838@gmail.com>
2024-07-01 22:52:49 +0530

    Add project files.

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,99 @@
+﻿using BulkyWebApp.Data;
+using BulkyWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWebApp.Controllers
+{
+	public class CategoryController : Controller
+	{
+		private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+		{
+			List<Category> objCategoryList = _db.Categories.ToList();
+			return View(objCategoryList);
+		}
+
+        public IActionResult Create()
+        {           
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("", "The Display Order or Name Must be Unique");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category Created Sucessfully";
+                return RedirectToAction("Index", "Category");
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int? Id)
+        {
+            if(Id== null || Id==0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(Id);
+            if(categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category Upadted Sucessfully";
+
+                return RedirectToAction("Index", "Category");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? Id)
+        {
+            if (Id == null || Id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(Id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost,ActionName("Delete")]
+        public IActionResult DeletePost(int? Id)
+        {
+            if (Id == null || Id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb=_db.Categories.Find(Id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(categoryFromDb);
+            _db.SaveChanges();
+            TempData["success"] = "Category Deleted Sucessfully";
+            return RedirectToAction("Index", "Category");
+        }
+    }
+}
---
+++ Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWebApp.Models
+{
+	public class Category
+	{
+        [Key]// if name is Id  model+Id=CategoryId then not required
+        public int Id { get; set; }
+        [Required]//not Null
+        [MaxLength(200)]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1,100,ErrorMessage = "The field Display Order must be between 1 and 100.")]
+        public int DisplayOrder { get; set; }   
+
+
+    }
+}
